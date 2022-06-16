4507a4031a675e99609e79f5cec7bdea49a2aea9
DenyAnwar <denyanwardev@gmail.com>
2022-06-16 18:07:11 +0800

    done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,7 @@
 // TODO: import module bila dibutuhkan di sini
+const { rejects } = require('assert');
+const fs = require('fs');
+const { resolve } = require('path');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +21,67 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const getExpectedWord = (words) => {
+  const splittedWord = words.split(' ');
+
+  if (splittedWord?.length >= 1) {
+    return splittedWord[1]
+  };
+};
+
+const processData = (dataSet) => {
+  const data = JSON.parse(dataSet);
+  let expectedString = '';
+
+  // case object
+  if (data?.message !== undefined) {
+    expectedString = data?.message;
+  };
+
+  // case array
+  if (data?.length) {
+    data?.forEach(item => {
+      if (item?.message !== undefined) {
+        expectedString = item?.message;
+      };
+
+      if (item?.data?.message !== undefined) {
+        expectedString = item?.data?.message;
+      }
+    });
+  };
+
+  return getExpectedWord(expectedString);
+};
+
+const bacaData = (fnCallback) => {
+  const fileList = [file1, file2, file3];
+  const result = [];
+
+  fileList.forEach(item => {
+      const processItem = new Promise((resolve, reject) => {
+        fs.readFile(item, (error, data) => {
+          if (error) {
+            reject(error);
+            return;
+          }
+
+          const getProcessedItem = processData(data);
+          resolve(getProcessedItem);
+        });
+      });
+
+      result.push(processItem);
+  });
+
+  // call result
+  Promise.all(result).then(values => {
+    fnCallback(null, values)
+  }).catch(error => {
+    fnCallback(error, null);
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
